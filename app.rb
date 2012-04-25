04365c7e99a56de44e208d49ab365f017e619c28
Thomas Bates <hola@ilovemonsters.co.uk>
2012-04-25 15:30:05 +0100

    Added configuration block
    A list of articles, author and site information is now initialised on
    start up (SETUP CONSTANT).

--- app.rb
+++ app.rb
@@ -8,74 +8,107 @@ require 'haml'
 require 'yaml'
 require 'rdiscount'
 
-# Set public and view directory
-set :public_folder, settings.root + '/assets'
-set :views, settings.root + '/views'
+# Configuration
+# This is run once at startup, Creating our SETUP
+# Constant, that contains a list of our articles.
+#
+configure do
+	# Set public and view directory
+	set :public_folder, settings.root + '/assets'
+	set :views, settings.root + '/views'
 
-# We want this to be super simple, even if it
-# could technically be done better.
-CONFIG = {
+	# We want this to be super simple, even if it
+	# could technically be done better.
+	SETUP = {
+		# All your general site information
+		:site_info => {
+			:title        => "I Love Monsters",
+			:url          => "http://blooming-leaf-7938.herokuapp.com",
+			:description  => "This is the description about your site. It's awesome right?",
+			:keywords     => ["something", "something else", "blog", "yeah", "like", "tags"]
+		},
 
-	# All your general site information
-	:site_info => {
-		:title        => "I Love Monsters",
-		:url          => "http://blooming-leaf-7938.herokuapp.com",
-		:description  => "This is the description about your site. It's awesome right?",
-		:keywords     => ["something", "something else", "blog", "yeah", "like", "tags"]
+		# All your author information
+		:author_info  => {
+			:name     => "Thomas Bates",
+
+			# External Links
+			# Req: Service title, your username and the service URL.
+			:external_links => [
+				{
+					:service  => "Twitter",
+					:username => "yoamomonstruos",
+					:service_url => "http://twitter.com"
+				},
+				{
+					:service  => "Github",
+					:username => "yoamomonstruos",
+					:service_url => "http://github.com"
+				},
+				{
+					:service  => "Dribbble",
+					:username => "yoamomonstruos",
+					:service_url => "http://dribbble.com"
+				}
+			]
+		},
+
+		# Store our articles dynamically sorta.
+		:articles => []
 	}
 
-	# All your author information
-}
+	# Read all files in articles directory
+	Dir.glob("articles/*") do |folder|
+		file = File.read("#{folder}/index.md").split("---\n")
+
+		# Load the articles meta
+		meta = YAML.load(file[1])
+
+		# Load the articles content
+		markdown = file[2]
+
+		# Setup the article object
+		article = {
+			title: meta['title'],
+			slug: folder.split('/')[1],
+			date: meta['date'],
+			tags: meta['tags'],
+			content: markdown
+		}
+
+		# Push the article to @articles array
+		SETUP[:articles] << article
+	end
+end
 
 helpers do
-
 	# Get Site title
 	def site_title
-		return CONFIG[:site_info][:title]
+		SETUP[:site_info][:title]
 	end
 
 	# Get Site URL
 	def site_url
-		return CONFIG[:site_info][:url]
+		SETUP[:site_info][:url]
 	end
 
 	# Get Site description
 	def site_description
-		return CONFIG[:site_info][:description]
+		SETUP[:site_info][:description]
 	end
 
 	# Get author info
-	def get_author
-
+	def author_name
+		SETUP[:author_info][:name]
 	end
 
-	# Get All The Articles
+	# External Links
+	def external_links
+		SETUP[:author_info][:external_links]
+	end
+
 	def get_articles
-		# Setup a blank array
-		@articles = []
-
-		# Read all files in articles directory
-		Dir.glob("articles/*") do |folder|
-			file = File.read("#{folder}/index.md").split("---\n")
-
-			# Load the articles meta
-			meta = YAML.load(file[1])
-
-			# Load the articles content
-			markdown = file[2]
-
-			# Setup the article object
-			article = {
-				title: meta['title'],
-				slug: folder.split('/')[1],
-				date: meta['date'],
-				tags: meta['tags'],
-				content: markdown
-			}
-
-			# Push the article to @articles array
-			@articles << article
-		end
+		@articles = SETUP[:articles]
 	end
 end
 
